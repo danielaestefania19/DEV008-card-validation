77bf3e229b23caecdde6484029b444bf42c553c6
danielaecm <danielacastellano1000@gmail.com>
2023-05-01 12:17:01 -0600

    Install Eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import validator from './validator.js';
+// iniciar el servidor en el puerto 5000
+/* const http = require('http');
 
-console.log(validator);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola, mundo!');
+});
+
+server.listen(5000, () => {
+  console.log('Servidor en ejecución en el puerto 5000');
+}); */
+
+
+// Función para manejar el envío del formulario
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,60 @@
-const validator = {
-  // ...
-};
+    
+          // Función para validar tarjetas de crédito
+          function validateCreditCardNumber(creditCardNumber) {
+            // Eliminar cualquier espacio en blanco y guiones
+            creditCardNumber = creditCardNumber.replace(/\s+/g, '').replace(/-/g, '');
+      
+            // Verificar si el número es válido
+            if (/^\d{13,16}$/.test(creditCardNumber)) {
+              // Calcular el dígito de verificación
+              var sum = 0;
+              var digit;
+              var addend;
+              var even = false;
+              for (var i = creditCardNumber.length - 1; i >= 0; i--) {
+                digit = parseInt(creditCardNumber.charAt(i));
+                if (even) {
+                  addend = digit * 2;
+                  if (addend > 9) {
+                    addend -= 9;
+                  }
+                } else {
+                  addend = digit;
+                }
+                sum += addend;
+                even = !even;
+              }
+              return (sum % 10) == 0;
+            } else {
+              return false;
+            }
+          }
+      
+          // Función para manejar el envío del formulario
+          function handleSubmit(event) {
+            event.preventDefault();
+            var creditCardNumber = document.getElementById("credit-card-number").value;
+            if (validateCreditCardNumber(creditCardNumber)) {
+              alert("El número de tarjeta de crédito es válido.");
+            } else {
+              alert("El número de tarjeta de crédito no es válido.");
+            }
+          }
+      
+          // Agregar un escucha de eventos al formulario
+          var form = document.querySelector("form");
+          form.addEventListener("submit", handleSubmit);
+      
+          // Agregar un escucha de eventos al campo de entrada
+          var creditCardNumberInput = document.getElementById("credit-card-number");
+          creditCardNumberInput.addEventListener("input", function() {
+            var maskedNumber = "";
+            var unmaskedLength = creditCardNumberInput.value.length - 4;
+            for (var i = 0; i < unmaskedLength; i++) {
+              maskedNumber += "*";
+            }
+            maskedNumber += creditCardNumberInput.value.substring(unmaskedLength);
+            creditCardNumberInput.value = maskedNumber;
+          });
 
-export default validator;
+      
